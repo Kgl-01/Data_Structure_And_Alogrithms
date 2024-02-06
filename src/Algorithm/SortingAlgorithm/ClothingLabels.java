6d9a50598c33444186beaff662c86d3913cabb74
Kgl-01 <karthikgowda.mailme@gmail.com>
2024-02-06 09:00:33 +0530

    added clothing labels algo

---
+++ src/Algorithm/SortingAlgorithm/ClothingLabels.java
@@ -0,0 +1,23 @@
+package Algorithm.SortingAlgorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClothingLabels {
+    public static void main(String[] args) {
+        String [] arr= {"Purple Shirt","Green Shirt"};
+
+        List <String> clothingOptions = markInventory(arr);
+        System.out.println(clothingOptions);
+    }
+
+    public static List<String>markInventory(String[]arr){
+        List <String> clothingOptions = new ArrayList<>();
+        for(int i = 0 ; i<arr.length ; i++){
+            for(int j = 1 ; j<=5 ; j++){
+                clothingOptions.add(arr[i]+"Size: "+j);
+            }
+        }
+        return clothingOptions;
+    }
+}
