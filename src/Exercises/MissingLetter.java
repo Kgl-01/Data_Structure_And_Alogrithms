e9c5141526228b7be1f3f1d87c3dfa5cec8ac32c
Kgl-01 <karthikgowda.mailme@gmail.com>
2024-02-10 10:31:14 +0530

    added missing letter algo

---
+++ src/Exercises/MissingLetter.java
@@ -0,0 +1,54 @@
+package Exercises;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MissingLetter {
+    public static void main(String[] args) {
+        String str = "the quick brown bix jumps over a lazy dog";
+        System.out.println(missingLetter(str));
+        System.out.println(missingLetterTwo(str));
+    }
+
+
+    static char missingLetterTwo(String str){
+        Map<Character,Boolean> map = new HashMap<>();
+        str=str.toLowerCase();
+
+        for(char c : str.toCharArray()){
+            if(Character.isLetter(c)){
+                map.put(c,true);
+            }
+        }
+
+        for(char c='a' ; c<='z' ;c++){
+            if(map.get(c)==null){
+                return c;
+            }
+        }
+
+        return '0';
+
+    }
+
+    /** Time Complexity 0(N)*/
+    static char missingLetter(String str){
+        boolean [] alphabetPresent = new boolean[26];
+        str=str.toLowerCase();
+
+        for(char c:str.toCharArray()){
+            if(Character.isLetter(c)){
+                int index = c-'a';
+                alphabetPresent[index]=true;
+            }
+        }
+
+        for(int i = 0 ; i<alphabetPresent.length ; i++){
+            if(!alphabetPresent[i]){
+                return (char)('a'+i);
+            }
+        }
+        return '0';
+    }
+}
