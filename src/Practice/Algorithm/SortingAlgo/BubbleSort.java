ec2522ddbf833751b44aaeb2a56d0e0168bbd65c
Kgl-01 <karthikgowda.mailme@gmail.com>
2024-03-11 09:06:58 +0530

    updated practice package

---
+++ src/Practice/Algorithm/SortingAlgo/BubbleSort.java
@@ -0,0 +1,46 @@
+package Practice.Algorithm.SortingAlgo;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] numbers = {4, 2, 7, 1, 3};
+//        bubbleSortOne(numbers);
+        bubbleSortTwo(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+
+    /*Approach One. Time Complexity O(N^2)*/
+    public static void bubbleSortOne(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void bubbleSortTwo(int[] arr) {
+        int unsortedUntilIndex = arr.length - 1;
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < unsortedUntilIndex; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+            unsortedUntilIndex--;
+        }
+    }
+}
---
+++ src/Practice/Exercises/Chapter04.java
@@ -0,0 +1,9 @@
+package Practice.Exercises;
+
+public class Chapter04 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
