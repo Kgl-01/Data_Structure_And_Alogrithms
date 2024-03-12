5923b1b4e0d6e645c730713bd296f2f999d02f84
Kgl-01 <karthikgowda.mailme@gmail.com>
2024-03-12 08:12:28 +0530

    added selection sorting practice

---
+++ src/Practice/Algorithm/SortingAlgo/SelectionSorting.java
@@ -0,0 +1,30 @@
+package Practice.Algorithm.SortingAlgo;
+
+import java.util.Arrays;
+
+public class SelectionSorting {
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 2, 7, 1, 3};
+        selectionSorting(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+
+    /*Time complexity O(N^2)*/
+    public static void selectionSorting(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int lowestValueIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[lowestValueIndex]) {
+                    lowestValueIndex = j;
+                }
+            }
+            if (lowestValueIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[lowestValueIndex];
+                arr[lowestValueIndex] = temp;
+            }
+        }
+    }
+}
